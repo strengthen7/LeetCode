a2e0703a176f244f92d93be0a810b4f20b3a7719
zhaojq <zhaojq@tsintergy.com>
2022-10-04 22:27:31 +0800

    feat:d1004 min valid parentheses

---
+++ src/com/anacmer/learn/leetcode/everyday/D1004MinValidParentheses.java
@@ -0,0 +1,16 @@
+package com.anacmer.learn.leetcode.everyday;
+
+public class D1004MinValidParentheses {
+
+    public int minAddToMakeValid(String s) {
+        while (s.contains("()")) {
+            s = s.replaceAll("\\(\\)", "");
+        }
+        return s.length();
+    }
+
+    public static void main(String[] args) {
+        D1004MinValidParentheses d1004MinValidParentheses = new D1004MinValidParentheses();
+        System.out.println(d1004MinValidParentheses.minAddToMakeValid("(()))"));
+    }
+}
